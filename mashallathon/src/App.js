11aaeb3b1838c456964eb07bda2db6c7aba0b9e3
Mutab sa <mutabsa@Mutabs-MacBook-Pro.local>
2021-01-07 14:52:16 +0300

    my comment Mutab

--- mashallathon/src/App.js
+++ mashallathon/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Chris from './Components/Chris/Chris.jsx';
 import Monica from './Components/Monica/Monica.jsx';
+import Mutab from './Components/Mutab/Mutab.jsx';
 
 function App() {
   return (
     <div className="flex">
       <Chris />
       <Monica />
+      <Mutab />
+      
     </div>
   );
 }
