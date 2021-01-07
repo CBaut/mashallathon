9075555d09cacfce6962f853dcd624cb2aa00073
Murtja Alkhabbaz <murtja.canada@gmail.com>
2021-01-07 14:34:14 +0300

    added my components

--- mashallathon/src/App.js
+++ mashallathon/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Chris from './Components/Chris/Chris.jsx';
 import Monica from './Components/Monica/Monica.jsx';
+import Murtaja from './Components/Murtaja/Murtaja.jsx';
 
 function App() {
   return (
     <div className="flex">
       <Chris />
       <Monica />
+      <Murtaja/>
     </div>
   );
 }
