1ed5e36f4a062f6d9e8869b4f8740a07ec6d0a7b
Alreem Alabbad <97abbad@gmail.com>
2021-01-07 15:02:42 +0300

    my comment

--- mashallathon/src/App.js
+++ mashallathon/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Chris from './Components/Chris/Chris.jsx';
 import Monica from './Components/Monica/Monica.jsx';
+import Alreem from './Components/Alreem/Alreem.jsx';
+import Mohammed from './Components/Mohammed/Mohammed.jsx';
+
 
 function App() {
   return (
     <div className="flex">
       <Chris />
       <Monica />
+      <Alreem />
+      <Mohammed/>
     </div>
   );
 }
