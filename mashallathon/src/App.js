ac638ade2a7471d82a5eb3079219663f49bd6fab
Monica Hong <momo.star@gmail.com>
2021-01-07 04:44:22 -0800

    Adding Susie and Abdullah

--- mashallathon/src/App.js
+++ mashallathon/src/App.js
@@ -2,11 +2,21 @@ import './App.css';
 import Chris from './Components/Chris/Chris.jsx';
 import Monica from './Components/Monica/Monica.jsx';
 
+
+
+
+
+import Susie from './Components/SA/Susie'
+import Abdullah from './Components/SA/Abdullah'
+
+
 function App() {
   return (
     <div className="flex">
       <Chris />
       <Monica />
+      <Susie/>
+      <Abdullah/>
     </div>
   );
 }
